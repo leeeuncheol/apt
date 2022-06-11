3b9a3c33bcf1ed1852fb83e1048e6b30cf4f5545
Lee <dan8792@gmail.com>
2022-06-11 23:54:41 +0900

    column added(desc, confrim date)

--- naver_crawling_2.py
+++ naver_crawling_2.py
@@ -6,22 +6,38 @@ import datetime as dt
 from sqlalchemy import create_engine
 import pymysql
 import os
+import webbrowser
 res = pd.DataFrame()
 
+#송정 
 #명촌돟 평창리비에르2차 : 16248
 #화봉동 쌍용예가 : 105026
+
+#문수로아이파크2차 1단지 : 104796
+#문수로아이파크2차 2단지 : 104797
+#대현 더샵 1 : 111347
+#대현 더샵 2 : 111348
 #번영로 하늘채 : 132862
 #야음 동부 : 6053
-#대현 더샵 2 : 111348
-#힐스이스턴베이 : 106557
+#쌍용스윗닷홈 : 12007
 #아데라움 : 12053
-
 #옥동대공원한신휴플러스 : 102949
+
+#약사더샵 
+#센트리지 : 135601
+
+#서부동 자이1 : 131665
+#서부동 자이1 : 131666
+#힐스이스턴베이 : 106557
+#빅쓰리
+
+#미장아이파크2 : 110132
 
 
 
 #단지 LIST
-aptArray = ['16248','105026','132862','6053','111348','106557','12053','102949']
+# aptArray = ['16248','105026','132862','6053', '111347', '111348','106557','12053','102949','110132','131665','131666','135601','12007', '104796', '104797']
+aptArray = ['132862']
 
 
 for i in range(0, len(aptArray)):
@@ -35,9 +51,15 @@ for i in range(0, len(aptArray)):
         headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.151 Whale/3.14.134.62 Safari/537.36'}
         response = requests.request("GET", url, headers=headers, data=payload)
 
-        # print(response.text)c
+        # print(response.text)
 
-        data = json.loads(response.text)
+        try : 
+            data = json.loads(response.text)
+        except : 
+            url = "https://m.land.naver.com/index"
+            webbrowser.open(url)
+            break
+        
         # print(data)
         items = data['result']['list']
         # print(items[0])
@@ -60,12 +82,14 @@ for i in range(0, len(aptArray)):
 
 #필요한 Column만 골라 씀
 # sortRes = res[['atclNm','bildNm', 'flrInfo', 'spc2','tradTpNm', 'prcInfo']].sort_values(by=['prcInfo'],ascending=False)
-finalRes = pd.DataFrame(res[['atclNm','bildNm', 'flrInfo', 'spc2','tradTpNm', 'prcInfo','atclNo']])
+finalRes = pd.DataFrame(res[['atclNm','bildNm', 'flrInfo', 'spc2','tradTpNm', 'prcInfo','atclNo','atclFetrDesc','cfmYmd']])
 finalRes['date'] = dt.datetime.now().date()
 finalRes['date'] = pd.to_datetime(finalRes['date'])
-#DB 또는 POWER BI에서 처리
+#POWER BI에서 처리
 # finalRes['atclNo'] = "https://m.land.naver.com/article/info/" + finalRes['atclNo']
 
+#호가 string type 그대로 따로 저장
+finalRes['호가'] = finalRes['prcInfo']
 
 # 면적, 가격은 Numeric으로 변환
 finalRes['spc2'] = pd.to_numeric(finalRes['spc2'])
@@ -86,7 +110,7 @@ def priceRe(x) :
             
             total = int(a) * 10000 + int(b.replace('/',''))
     else:
-
+        # 월세는 '/'기호 우선 999로 대체
         if '/' in x : 
             total = int(x.replace('/','999'))      
         else :    
