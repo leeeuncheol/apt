e646e73f9fd1c7ebf941eb48223b4f72bdf80e94
Lee <dan8792@gmail.com>
2022-06-11 23:57:49 +0900

    중복값 및 최고가 오류 수정

--- api_2.py
+++ api_2.py
@@ -1,3 +1,4 @@
+from unittest import skip
 import pandas as pd
 import numpy as np
 import requests
@@ -6,8 +7,10 @@ import os
 import xml.etree.ElementTree as ET
 from sqlalchemy import create_engine
 import pymysql
+import decimal
 import random
 import string
+from datetime import date
 
 #API 호출에 법정동코드 앞 5자리가 필요
 code_file = "법정동코드 전체자료.txt"
@@ -30,8 +33,8 @@ gu_code_list = subset_df['substr'][1:].reset_index(drop = True)
 
 
 #추출 하고자하는 기간 설정
-year = [str("%02d" %(y)) for y in range(2022, 2023)]
-month = [str("%02d" %(m)) for m in range(6, 7)]
+year = [str("%02d" %(y)) for y in range(2010, 2023)]
+month = [str("%02d" %(m)) for m in range(1, 13)]
 base_date_list = ["%s%s" %(y, m) for y in year for m in month ]
 
 
@@ -86,15 +89,13 @@ items = pd.DataFrame(items_list)
 
 ## data type 변경
 items['거래금액'] = pd.to_numeric(items['거래금액'].str.replace(',',''))
-items['전용면적'] = pd.to_numeric(items['전용면적'])
+items['전용면적'] = round(pd.to_numeric(items['전용면적']), 2)
 items['년'] = pd.to_numeric(items['년'])
 items['월'] = pd.to_numeric(items['월'])
 items['일'] = pd.to_numeric(items['일'])
 items['층'] = pd.to_numeric(items['층'])
 items['건축년도'] = pd.to_numeric(items['건축년도'])
-items['등록일자'] = pd.datetime.now().date()
-
-
+items['등록일자'] = date.today()
 items['지역코드'] = items['지역코드'] + '00000'
 
 gu_code_name = code[code['code'].isin(items['지역코드'])][['code','name']]
@@ -103,65 +104,15 @@ gu_name_dict = gu_code_name.set_index('code').T.to_dict('index')['name']
 items = items.replace({'지역코드' : gu_name_dict })
 
 #data check
+print('***********************QUERY DATA************************')
 print(items.head())
+print('*********************************************************')
 
 
 #CSV파일 저장
 # items.to_csv(os.path.join("%s_%s~%s.csv" %(gu, year[0], year[-1])), index=False,encoding="euc-kr") 
 
 
-#DB 저장 
-# MySQL Connector using pymysql
-
-
-pymysql.install_as_MySQLdb()
-
-engine = create_engine('mysql+mysqlconnector://crawl_user:test001@localhost:3306/crawl_data', encoding='utf-8')
-conn = engine.connect()
-
-
-
-"""
-def table_column_names(table: str) -> str:
- 
-    query = f"SELECT column_name FROM information_schema.columns WHERE table_name = '{table}'"
-    rows = engine.execute(query)
-    dirty_names = [i[0] for i in rows]
-    clean_names = '`' + '`, `'.join(map(str, dirty_names)) + '`'
-    return clean_names
-
-
-
-def insert_conflict_ignore(df: pd.DataFrame, table: str, index: bool):
-    
-    # generate random table name for concurrent writing
-    temp_table = ''.join(random.choice(string.ascii_letters) for i in range(10))
-    try:
-        df.to_sql(temp_table, engine, index=index)
-        columns = table_column_names(table=temp_table)
-        insert_query = f'INSERT IGNORE INTO {table}({columns}) SELECT {columns} FROM `{temp_table}`'
-        engine.execute(insert_query)
-    except Exception as e:
-        print(e)        
-
-    # drop temp table
-    drop_query = f'DROP TABLE IF EXISTS `{temp_table}`'
-    engine.execute(drop_query)
-
-
-def save_dataframe(df: pd.DataFrame, table: str):
-   
-    if df.index.name is None:
-        save_index = False
-    else:
-        # save_index = True
-        save_index = False
-
-    insert_conflict_ignore(df=df, table=table, index=save_index)
-
-
-save_dataframe(items, 'apt2')
-"""
 
 # DB에 중복되면 안되는 열들을 제외하고 새로 추가해야 할 Data들만 업데이트하기
 # df: 새로 업데이트 할 Pandas DataFrame
@@ -195,43 +146,67 @@ def filter_new_df(df, tablename, engine, dup_cols=[],
     elif args_cat_filter:
         args += ' Where ' + args_cat_filter
 
+
+    currentDB = pd.read_sql(args, engine)
+    currentDB['전용면적'] = round(pd.to_numeric(currentDB['전용면적']), 2)
+    print('***********************EXIST DATA***********************')
+    print(currentDB.head())
+    print('*********************************************************')
+    df.drop_duplicates(dup_cols, keep='last', inplace=True)
+    df = pd.merge(df, currentDB, how='left', on=dup_cols, indicator=True)
+    df = df[df['_merge'] == 'left_only']
+    df.drop(['_merge'], axis=1, inplace=True)
+    
+    """
+    # 신고가 컬럼 기본값 설정
+    # df['신고가'] = 'X'
+    
     # 기존 최고가 
     oridf = pd.read_sql(args, engine)
     maxoridf = oridf.groupby(['아파트','전용면적'])['거래금액'].agg(**{'최고가':'max'}).reset_index()
     # print(maxoridf)
     
-    df.drop_duplicates(dup_cols, keep='last', inplace=True)
-    df = pd.merge(df, pd.read_sql(args, engine), how='left', on=dup_cols, indicator=True)
-    df = df[df['_merge'] == 'left_only']
-    df.drop(['_merge'], axis=1, inplace=True)
-    
-    df['전용면적'] = np.round(df['전용면적'], decimals = 0)
-    df['신고가'] = 'X'
-    
     for i in df.index : 
-        try : 
-            condition1 = maxoridf['아파트'] == df['아파트'][i] 
-            condition2 =  maxoridf['전용면적'] == df['전용면적'][i]
-            existMax = maxoridf.loc[condition1 & condition2]['최고가'].item()
+            try : 
+                condition1 = maxoridf['아파트'] == df['아파트'][i] 
+                condition2 =  maxoridf['전용면적'] == df['전용면적'][i]
+                existMax = maxoridf.loc[condition1 & condition2]['최고가'].item()
+            except : 
+                print('전고가 없음 :', df['아파트'][i], df['전용면적'][i])
+                existMax = 0
+            
             print('기존:', existMax, '/ 신규:' , df['거래금액'][i].item())
             
             if df['거래금액'][i].item() > existMax : 
-                df['신고가'][i] = 'O'
-        except : 
-            print('오류발생 :', df['아파트'][i], df['전용면적'][i])
-            
-    print('*****************') 
-    print('신고가율:', len(df[df['신고가'] == 'O'].index) / len(df.index) * 100, '%')
-    print('*****************')     
+                # df['신고가'][i] = 'O'
+                df.at[i, '신고가'] = 'O'
     
+    AllTradeCount =  len(df.index)                                
+    isHigh = df['신고가'] == 'O'
+    HighTradeCount =  len(df[isHigh].index)           
+    print('***********************') 
+    print('울산 전체 신고가율:', round(HighTradeCount / AllTradeCount * 100, 2), '%')
+    print( '신고가건수:',  HighTradeCount , '전체거래건수: ' , AllTradeCount )
+    print('***********************')     
+    """
     return df
 
 
+# MySQL Connector using pymysql
+pymysql.install_as_MySQLdb()
+engine = create_engine('mysql+mysqlconnector://crawl_user:test001@localhost:3306/crawl_data', encoding='utf-8')
+conn = engine.connect()
+
+#중복제거 함수 적용
 cols = ['거래금액','아파트','층','월','일','전용면적']
 newitems = filter_new_df(items, 'apt2', engine, cols, None, None)
+print('***********************UPDATE DATA************************')
 print(newitems)
+print('**********************************************************')
 
+# CSV 파일 생성
 # newitems.to_csv('c:/temp/test.csv', index=False)
 
+#DB 저장 
 newitems.to_sql(name='apt2', con=engine, if_exists='append', index = False)
 conn.close()
---
+++ 실행_네이버.bat
@@ -0,0 +1,3 @@
+@echo off
+"C:\Users\lec17\AppData\Local\Programs\Python\Python39\python.exe" "C:\code\apt\naver_crawling_2.py"
+pause
